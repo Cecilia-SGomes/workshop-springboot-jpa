f031ddb8f4d9b4708c3ab088fc76e0852ed54b68
Cecilia-SGomes <cecilia.gomes@academico.ifpb.edu.br>
2025-06-20 15:38:32 -0300

    feat: OrderItem, many-tomany association with extra attributes

--- src/main/java/com/maria/course/entities/Order.java
+++ src/main/java/com/maria/course/entities/Order.java
@@ -2,6 +2,8 @@ package com.maria.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.maria.course.entities.enums.OrderStatus;
@@ -12,6 +14,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,16 +35,19 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
+
+    public Order() {
+
+    }
+
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         super();
         this.id = id;
         this.moment = moment;
         setOrderStatus(orderStatus);
         this.client = client;
-    }
-
-    public Order() {
-
     }
 
     public Long getId() {
@@ -76,6 +82,10 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
