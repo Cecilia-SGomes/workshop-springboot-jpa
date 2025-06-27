a2d8b789cde5dc506c645c02d063512e0ebd3c6e
Cecilia-SGomes <cecilia.gomes@academico.ifpb.edu.br>
2025-06-27 15:05:28 -0300

    feat: Payment, one-to-one association

--- src/main/java/com/maria/course/config/TestConfig.java
+++ src/main/java/com/maria/course/config/TestConfig.java
@@ -11,10 +11,12 @@ import org.springframework.context.annotation.Profile;
 import com.maria.course.entities.Category;
 import com.maria.course.entities.Order;
 import com.maria.course.entities.OrderItem;
+import com.maria.course.entities.Payment;
 import com.maria.course.entities.Product;
 import com.maria.course.entities.User;
 import com.maria.course.entities.enums.OrderStatus;
 import com.maria.course.repositories.CategoryRepository;
+import com.maria.course.repositories.OrderItemRepository;
 import com.maria.course.repositories.OrderRepository;
 import com.maria.course.repositories.ProductRepository;
 import com.maria.course.repositories.UserRepository;
@@ -34,6 +36,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -75,6 +80,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 
 }
